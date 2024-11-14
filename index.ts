1531b3ebbb4a6a41d9cc52397609814201da8e5a
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-11-14 16:06:03 +0700

    Cấu hình

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-console.log("ok")
+import express, { Express, Request, Response } from "express";
+const app: Express = express();
+const port: Number = 3000;
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connect } from "./config/database";
+connect();
+
+import { Task } from "./models/task.model";
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+  res.json(tasks);
+});
+
+app.get("/tasks/detail/:id", async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  const task = await Task.findOne({
+    _id: id,
+    deleted: false,
+  });
+  res.json(task);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
---
+++ models/task.model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const taskSchema = new mongoose.Schema(
+  {
+    title: String,
+    status: String,
+    content: String,
+    timeStart: Date,
+    timeFinish: Date,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+export const Task = mongoose.model("Task", taskSchema, "tasks");
